ef2a786ffea3cf9e2590b59eb5a26f495d3bc7b9
Sergey Sukharev <i.mr.two@gmail.com>
2021-02-13 23:13:15 +0300

    Added QR code scanner library

--- app/src/main/java/dev/sukharev/clipangel/MainActivity.kt
+++ app/src/main/java/dev/sukharev/clipangel/MainActivity.kt
@@ -1,9 +1,33 @@
 package dev.sukharev.clipangel
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Size
+import android.view.TextureView
+
+import android.widget.Button
+import androidx.camera.core.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    companion object {
+        private const val REQUEST_CAMERA_PERMISSION = 10
+    }
+
+    private lateinit var textureView: TextureView
+
+    private lateinit var buttonPlus: Button
+
+    val previewConfig = PreviewConfig.Builder()
+            // We want to show input from back camera of the device
+            .setLensFacing(CameraX.LensFacing.BACK)
+            .setTargetResolution(Size(2000, 1500))
+            .build()
+
+    val preview = Preview(previewConfig)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
